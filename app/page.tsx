2522d3665c1dcf447fbed39baaad27b686ce07dc
Risal Walangadi <risal.walangadi@gmail.com>
2026-01-03 18:42:10 +0800

    change navbar to fixed

--- app/page.tsx
+++ app/page.tsx
@@ -29,16 +29,14 @@ export default function Home() {
       id: '3',
       text: 'Belajar NestJS',
       status: 'active'
-    },
+    }
   ]);
 
   return (
-    <div className="flex flex-col max-w-3xl mx-auto min-h-screen items-center bg-zinc-50 font-sans dark:bg-background">
-      <header className="flex flex-col w-full px-6 py-4 mx-auto">
-        <Navbar />
-      </header>
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-start bg-white dark:bg-background sm:items-start">
-        <div className="flex flex-col justify-center items-start gap-9 mx-auto ">
+    <div className="flex flex-col items-center bg-zinc-50 font-sans dark:bg-background mb-14">
+      <Navbar />
+      <main className="pt-16">
+        <div className="flex flex-col justify-center items-start gap-9 mx-auto">
           <div className="w-fit">
             <TodoSummary list={todo} />
           </div>
--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -2,17 +2,19 @@ import { LogIn } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center w-full  px-4 py-3">
-      <div className="font-bold relative group">
-        <a href="/">
-          Todo<span className="text-papaya">App</span>
-          <span className="absolute -bottom-0.5 left-0 h-0.5 w-0 bg-papaya transition-all duration-200 ease-out group-hover:w-full"></span>
-        </a>
-      </div>
-      <div>
-        <a href="#" className="hover:text-papaya transition-colors duration-200 ease-out">
-          <LogIn />
-        </a>
+    <nav className="fixed top-0 inset-x-0 z-50 bg-background">
+      <div className="mx-auto max-w-5xl px-36 flex items-center justify-between h-14">
+        <div className="font-bold relative group">
+          <a href="/">
+            Todo<span className="text-papaya">App</span>
+            <span className="absolute -bottom-0.5 left-0 h-0.5 w-0 bg-papaya transition-all duration-200 ease-out group-hover:w-full"></span>
+          </a>
+        </div>
+        <div>
+          <a href="#" className="hover:text-papaya transition-colors duration-200 ease-out">
+            <LogIn />
+          </a>
+        </div>
       </div>
     </nav>
   )
